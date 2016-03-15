a3eb9bbd8864406002f23e4efe5c93545a8b50b3
kai <kai.kruse@mpi-muenster.mpg.de>
2016-03-15 11:35:28 +0100

    Read regions from file

--- tadtool/tad.py
+++ tadtool/tad.py
@@ -18,3 +18,31 @@ class HicMatrixFileReader(object):
         else:
             raise ValueError("Either provide a file name or explicitly use 'load' before requesting matrix")
         return self.m
+
+
+class HicRegionFileReader(object):
+    def __init__(self, file_name=None, _separator="\t"):
+        self.sep = _separator
+        self.r = None
+        if file_name is not None:
+            self.load(file_name)
+
+    def load(self, file_name):
+        regions = []
+        with open(file_name, 'r') as f:
+            for line in f:
+                line = line.rstrip()
+                fields = line.split(self.sep)
+                if len(fields) > 2:
+                    chromosome = fields[0]
+                    start = int(fields[1])
+                    end = int(fields[2])
+                    regions.append([chromosome, start, end])
+        self.r = regions
+
+    def regions(self, file_name=None):
+        if self.r is None and file_name is not None:
+            self.load(file_name)
+        else:
+            raise ValueError("Either provide a file name or explicitly use 'load' before requesting regions")
+        return self.r
