00e00c1f70693d7ff22d48c35342826ad9dd46e3
kai <kai.kruse@mpi-muenster.mpg.de>
2016-03-30 14:56:58 +0200

    Modified for PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tadtool',
-    version='0.4.0',
+    version='0.4',
     description='Assistant to find cutoffs in TAD calling algorithms.',
     packages=find_packages(exclude=["test"]),
     install_requires=[
@@ -10,6 +10,9 @@ setup(
         'matplotlib',
         'progressbar2'
     ],
-
+    url='https://github.com/vaquerizaslab/tadtool',
+    download_url='https://github.com/vaquerizaslab/tadtool/tarball/0.4',
+    keywords=['bioinformatics', 'hi-c', 'genomics', 'tad'],
+    classifiers=[],
     scripts=['bin/tadtool']
 )
