3586deac5c2a956b6a8b3f0bd5c39779bb0ac97e
kai <kai.kruse@mpi-muenster.mpg.de>
2016-03-31 14:53:01 +0200

    Better example

--- examples/chr12_plot.py
+++ examples/chr12_plot.py
@@ -1,24 +1,16 @@
 import tadtool.tad as tad
 import tadtool.plot as tp
 
-# regions = tad.HicRegionFileReader().regions("/Users/kkruse1/Desktop/kaic-demo/tadtool/regions_chr12_20-35Mb.txt")
-# matrix = tad.HicMatrixFileReader().matrix("/Users/kkruse1/Desktop/kaic-demo/tadtool/chr12_20-35Mb.txt")
+# load regions data set
+regions = tad.HicRegionFileReader().regions("chr12_20-35Mb_regions.bed chr12_plot.py")
 
+# load matrix
+matrix = tad.HicMatrixFileReader().matrix("chr12_20-35Mb.matrix.txt")
 
-# da, ws = tad.data_array(matrix, regions)
-# dap = tp.DataArrayPlot(da, ws, regions)
-# fig, ax = dap.plot('chr12:25000000-30000000')
-# fig.show()
-regions = tad.HicRegionFileReader().regions("/Users/kkruse1/Desktop/kaic-demo/tadtool/chr12.10kb.regions.txt")
-matrix = tad.HicMatrixFileReader().matrix("/Users/kkruse1/Desktop/kaic-demo/tadtool/chr12.10kb.txt")
-
-# regions = tad.HicRegionFileReader().regions("/Users/kkruse1/Desktop/kaic-demo/tadtool/regions.txt")
-# matrix = tad.HicMatrixFileReader().matrix("/Users/kkruse1/Desktop/kaic-demo/tadtool/chr12.mat")
-
-ii = tp.insulation_index(matrix, regions, window_size=500000)
-r = tad.call_tads_insulation_index(ii, 0.02, regions)
-
-tad_plot = tp.TADtoolPlot(matrix, regions, norm='lin', max_dist=3000000, algorithm='insulation')
+# prepare plot
+tad_plot = tp.TADtoolPlot(matrix, regions, norm='lin', max_dist=1000000, algorithm='insulation')
 fig, axes = tad_plot.plot('chr12:31000000-34000000')
+
+# show plot
 fig.show()
 
