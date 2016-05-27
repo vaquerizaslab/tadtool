3f51828e04cc452c25100c1084d6f00ccdd6ba12
kai <kai.kruse@mpi-muenster.mpg.de>
2016-05-27 14:46:36 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tadtool',
-    version='0.5',
+    version='0.6',
     description='Assistant to find cutoffs in TAD calling algorithms.',
     packages=find_packages(exclude=["test"]),
     install_requires=[
@@ -13,7 +13,7 @@ setup(
     author='Vaquerizas lab',
     author_email='kai.kruse@mpi-muenster.mpg.de',
     url='https://github.com/vaquerizaslab/tadtool',
-    download_url='https://github.com/vaquerizaslab/tadtool/tarball/0.5',
+    download_url='https://github.com/vaquerizaslab/tadtool/tarball/0.6',
     keywords=['bioinformatics', 'hi-c', 'genomics', 'tad'],
     classifiers=[],
     scripts=['bin/tadtool']
