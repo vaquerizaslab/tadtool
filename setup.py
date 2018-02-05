2b141d35714c01bc5b5f71db44f9f709cf71ea8f
kai <kai.kruse@mpi-muenster.mpg.de>
2018-02-05 12:31:07 +0100

    Option to write insulation index values to file instead of tads

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tadtool',
-    version='0.75',
+    version='0.76',
     description='Assistant to find cutoffs in TAD calling algorithms.',
     packages=find_packages(exclude=["test"]),
     install_requires=[
@@ -14,7 +14,7 @@ setup(
     author='Vaquerizas lab',
     author_email='kai.kruse@mpi-muenster.mpg.de',
     url='https://github.com/vaquerizaslab/tadtool',
-    download_url='https://github.com/vaquerizaslab/tadtool/tarball/0.75',
+    download_url='https://github.com/vaquerizaslab/tadtool/tarball/0.76',
     keywords=['bioinformatics', 'hi-c', 'genomics', 'tad'],
     classifiers=[],
     scripts=['bin/tadtool']
